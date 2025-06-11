1f8f410b76064936ccb63bfe94e844ee9508de92
Boya-Raghavendra <raghavendra.achieversit@gmail.com>
2025-06-11 13:13:49 +0530

    Operators

---
+++ Theory/Operators/Operators.js
@@ -0,0 +1,128 @@
+//Operator
+//Operstors ara a special Sign s or key words that are perform a operation B?W the two operands
+
+
+//Why
+//1. Mathemetical calculation(calucation)
+//2. make comparision
+//3. 
+//1. Arithmetic
+//+,-,*,/,%,**
+
+//2. Assignment 
+
+//=,+=,-=,*=,/=,%=
+
+let a = 10;
+a %= 2;
+a = a%2;
+
+
+//Comparision 
+
+//==,===,<,>,<=,>=,!=
+
+//Logical
+
+//&&,||,!
+
+//Ternory:
+
+
+//Condition ? exp1:exp2
+
+
+let age = 18;
+let result = age>=18? "Eligible to VOTE" : "not eligible";
+console.log(result);
+
+
+let b = 10;
+let c = "10";
+
+console.log(b+c);//true
+
+//type of operator
+
+//let userData = prompt("Enter You age:")
+
+console.log(typeof userData);
+
+console.log(typeof NaN);
+
+
+//type of conversions
+// inmplicit type
+//explicit type
+//Number()
+//parseInt()
+//ParseFloat()
+
+
+Number(123)//123
+parseInt("12.56")//12
+parseFloat("10.000")//10.000
+
+
+//String()
+//valu.toString()
+
+String(123)//"123"
+
+//Boolean
+
+//0,"",null,undefined,fflse
+
+console.log("abc" + "3");
+
+console.log(Number("hello"));
+console.log(parseInt("xyz"));
+
+let x;
+console.log(x + 10);
+console.log(0 / 0);
+
+console.log(NaN === NaN);
+
+console.log(1 + 2 + '3');
+
+console.log('5' - 2);
+
+console.log(true + false);
+
+// console.log([] === false);//
+let rr= 0.1 + 0.2;
+console.log(rr);
+console.log( rr === 0.3);
+
+console.log("5" + 1 * 2);
+
+let result = 10 > 5 ? 10 > 7 ? 'A' : 'B' : 'C';
+console.log(result);
+
+//Increment
+//pre =>++a
+//post ==>a++
+
+
+let z = 10;
+console.log(z++);
+console.log(++z);//11
+//
+
+let a = 5;
+let b = a++;
+console.log(a, b);
+
+
+
+
+
+
+
+
+
+
+
+
+
